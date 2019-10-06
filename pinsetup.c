ee67b65e8ba568d47fc447616c8e0be1e2adcf3f
pstadler1990 <pstadler90@gmx.de>
2019-10-06 22:54:27 +0200

    initial commit of pin setup

---
+++ pinsetup.c
@@ -0,0 +1,20 @@
+//
+// Created by pstadler on 06.10.19.
+//
+
+#include "pinsetup.h"
+#include "pigpio.h"
+
+int8_t
+init_GPIOs(void) {
+    /* */
+    // TODO: Initialize PIGPIO library
+
+    // TODO: Configure pins for exchange with external uC (STM8)
+    // TODO: <- pin ready
+    // TODO: -> pin complete
+    // TODO: <- pin calib mode
+
+    // TODO: Add USART for intercommunication with external uC (for measurement values for calibration)
+    return 1;
+}
---
+++ pinsetup.h
@@ -0,0 +1,12 @@
+//
+// Created by pstadler on 06.10.19.
+//
+
+#ifndef FB_PINSETUP_H
+#define FB_PINSETUP_H
+
+#include <stdint.h>
+
+int8_t init_GPIOs(void);
+
+#endif //FB_PINSETUP_H
