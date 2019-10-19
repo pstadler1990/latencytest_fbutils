c47c9784af7f3d8241f57acf61cbee2627f77f8f
pstadler1990 <pstadler90@gmx.de>
2019-10-19 18:09:30 +0200

    added timeout and max failures

--- configuration.h
+++ configuration.h
@@ -6,6 +6,8 @@
 #define FB_CONFIGURATION_H
 
 #define DEFAULT_N_MEASUREMENTS      ((uint32_t)100)         /* Number of single measurements per series */
+#define MAX_FAILED_MEASUREMENTS     ((uint32_t)10)          /* Maximum number of failed measurements within a single test */
+#define MEASUREMENT_TIMEOUT         ((uint32_t)1000000)     /* Maximum timeout */
 
 /* GPIOs */
 #define GPIO_EXT_TRIGGER_IN         ((unsigned int)5)       /* Physical pin 29, BCM pin 5 */
