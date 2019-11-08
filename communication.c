1fb9074fdec8ecf259610ae430ade8ddd88ff948
pstadler1990 <pstadler90@gmx.de>
2019-11-08 15:02:38 +0100

    added helpers for sending and receiving

--- communication.c
+++ communication.c
@@ -2,11 +2,13 @@
 // Created by pstadler on 25.10.19.
 //
 #include "communication.h"
+#include "configuration.h"
 #include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
+#include <memory.h>
 
 extern int uart0_filestream;
 
@@ -53,4 +55,56 @@ uart_receive(char* buf, size_t len) {
         return (int) receivedBytes;
     }
     return -1;
+}
+
+uint8_t
+uart_send_command(UART_CTRL_COMMAND command) {
+    uint8_t buf[2] = {
+        command,
+        '\n'
+    };
+
+    int32_t receiveStatus = -1;
+    uint32_t m_timeout = MEASUREMENT_TIMEOUT;
+
+    /* Wait for trigger reception from STM8 to enable ADC mode */
+    while(1) {
+        char receiveBuf[10];
+
+        uart_send(buf, 2);
+
+        receiveStatus = uart_receive(receiveBuf, 10);
+        if(receiveStatus) {
+            if(strncmp("OK", receiveBuf, 2) == 0) {
+                return 1;
+            }
+        }
+        if(--m_timeout == 0) {
+            printf("Failed to send command, exit\n");
+            break;
+        }
+        sleep(1);
+    }
+    return 0;
+}
+
+uint8_t
+uart_receive_response(char* receiveBuf, const uint32_t responseLen) {
+    /* */
+    int32_t receiveStatus = -1;
+    uint32_t m_timeout = MEASUREMENT_TIMEOUT;
+
+    while(receiveStatus == -1) {
+        printf(".\r\n");
+
+        receiveStatus = uart_receive(receiveBuf, responseLen);
+        if(receiveStatus) {
+            return 1;
+        }
+
+        if(--m_timeout == 0) {
+            break;
+        }
+    }
+    return 0;
 }
--- communication.h
+++ communication.h
@@ -8,7 +8,19 @@
 #include <stdint.h>
 #include <glob.h>
 
+typedef enum{
+    CTRL_CMD_ADC_MODE = 'A',
+    CTRL_CMD_DIGIT_MODE = 'D',
+    CTRL_CMD_CALIB_MODE = 'C',
+    CTRL_CMD_GET_MEASURES = 'M',
+    CTRL_CMD_CALIB_BLACK = 'B',
+    CTRL_CMD_CALIB_WHITE = 'W',
+
+} UART_CTRL_COMMAND;
+
 uint8_t init_uart(const char* uartIdentifier);
 uint8_t uart_send(const uint8_t* buf, size_t len);
 int uart_receive(char* buf, size_t len);
+uint8_t uart_send_command(UART_CTRL_COMMAND command);
+uint8_t uart_receive_response(char* receiveBuf, uint32_t responseLen);
 #endif //FB_COMMUNICATION_H
