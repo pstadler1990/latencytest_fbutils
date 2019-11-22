cc67739e0c9182f662d24cea8fc71a4e98ce9f8d
pstadler1990 <pstadler90@gmx.de>
2019-11-22 12:30:13 +0100

    removed unused pins and changed measurement number back to 100

--- configuration.h
+++ configuration.h
@@ -5,7 +5,7 @@
 #ifndef FB_CONFIGURATION_H
 #define FB_CONFIGURATION_H
 
-#define DEFAULT_N_MEASUREMENTS      ((uint32_t)20)        /* Number of single measurements per series */
+#define DEFAULT_N_MEASUREMENTS      ((uint32_t)100)        /* Number of single measurements per series */
 #define MAX_FAILED_MEASUREMENTS     ((uint32_t)10)         /* Maximum number of failed measurements within a single test */
 #define MEASUREMENT_TIMEOUT         ((uint32_t)1000000)    /* Maximum timeout */
 #define ROT_DEBOUNCE_TIME           ((uint32_t)100)
@@ -17,8 +17,6 @@
 /* GPIOs */
 #define GPIO_EXT_TRIGGER_IN         ((unsigned int)5)       /* Physical pin 29, BCM pin 5 */
 #define GPIO_EXT_TRIGGER_OUT        ((unsigned int)17)      /* Physical pin 11, BCM pin 17 */
-#define GPIO_EXT_MODE_ADC_OUT       ((unsigned int)27)      /* Physical pin 13, BCM pin 27*/
-#define GPIO_EXT_MODE_DIGITAL_OUT   ((unsigned int)22)      /* Physical pin 15, BCM pin 22*/
 
 #define GPIO_INT_ROT_A              ((unsigned int)26)      /* Physical pin 37, BCM pin 26*/
 #define GPIO_INT_ROT_B              ((unsigned int)19)      /* Physical pin 35, BCM pin 19*/
